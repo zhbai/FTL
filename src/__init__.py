ce48d2e88f14b87d8610bfc19d8fd117f1a0e90e
zhbai <zhebai@lbl.gov>
2024-06-13 13:17:42 -0700

    direcotry file

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+from .encoder import Encoder
+from .decoder import Decoder
+from .utils import train_epoch
+from .utils import test_epoch
+from .utils import checkpoint
+from .utils import plot_ae_outputs
+from .utils import load_trained_network
+
+# package metadata
+__version__ = '1.0'
+__license__ = 'modified BSD license'
