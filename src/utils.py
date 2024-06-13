77776f6782d6f1570fb40c074ec8022457f351d8
zhbai <zhebai@lbl.gov>
2024-06-13 13:19:11 -0700

    update utils

--- src/utils.py
+++ src/utils.py
@@ -1,92 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import torch
-from torch import nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader, random_split
+import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings('ignore')
 
-class Encoder(nn.Module):
-    
-    def __init__(self, encoded_z_dim, fc2_in_dim):
-        super().__init__()
-        
-        ## convolutional section
-        self.encoder_cnn = nn.Sequential(
-            # 1st convolutional layer
-            nn.Conv2d(1, 8, 3, stride=3, padding=1), # 8, (101-3+2+3)/3 = 34, (181-3+2+3)/3 = 61
-            # nn.BatchNorm2d(8),
-            nn.ReLU(True),
-            # 2nd convolutional layer 
-            nn.Conv2d(8, 16, 3, stride=3, padding=1), # 16, (34-3+2+3)/3 = 12, (61-3+2+3)/3 = 21   
-            nn.BatchNorm2d(16),
-            nn.ReLU(True),
-            # 3rd convolutional layer
-            nn.Conv2d(16, 32, 3, stride=2, padding=0), # 32, (12-3+2)/2 = 5, (21-3+2)/2 = 10
-            nn.ReLU(True)
-        )
-        ## flatten layer
-        self.flatten = nn.Flatten(start_dim=1)
-        
-        ## linear section
-        self.encoder_lin = nn.Sequential(
-            # 1st linear layer
-            nn.Linear(5 * 10 * 32, fc2_in_dim),  
-            # hyperbolic tangent
-            nn.Tanh(),
-            # 2nd linear layer
-            nn.Linear(fc2_in_dim, encoded_z_dim)
-        )
-        
-    def forward(self, x):
-        # encoder conv layers 
-        x = self.encoder_cnn(x)
-        # flatten
-        x = self.flatten(x)
-        # encoder linear layers
-        x = self.encoder_lin(x)
-        return x
-    
-class Decoder(nn.Module):
-    
-    def __init__(self, encoded_z_dim, fc2_in_dim):
-        super().__init__()
-        
-        self.decoder_lin = nn.Sequential(
-            # 1st linear layer
-            nn.Linear(encoded_z_dim, fc2_in_dim),
-            # hyperbolic tangent
-            nn.Tanh(),
-            # 2nd linear layer
-            nn.Linear(fc2_in_dim, 5 * 10 * 32),
-            nn.ReLU(True)
-        )
-        
-        # unflatten
-        self.unflatten = nn.Unflatten(dim=1, unflattened_size=(32, 5, 10))
-
-        self.decoder_conv = nn.Sequential(
-            # 2D transposed convolution operator 
-            nn.ConvTranspose2d(32, 16, 3, stride=2, output_padding= (1, 0)), # 5*2-2+3+1 = 12, 10*2-2+3 = 21  
-            nn.BatchNorm2d(16),
-            nn.ReLU(True),
-            # 2nd transposed convolution operator 
-            nn.ConvTranspose2d(16, 8, 3, stride=3, padding=1), # 12*3-3-2+3 = 34, 21*3-3-2+3 = 61 
-            nn.BatchNorm2d(8),
-            nn.ReLU(True),
-            # 3rd transposed convolution operator 
-            nn.ConvTranspose2d(8, 1, 3, stride=3, padding=1, output_padding= (1, 0)) # 34*3-3-2+3+1= 101, 61*3-3-2+3 = 181
-        )
-        
-    def forward(self, x):
-        # decoder linear layers
-        x = self.decoder_lin(x)
-        # unflatten
-        x = self.unflatten(x)
-        # transposed convolutions
-        x = self.decoder_conv(x)
-        return x
     
 def train_epoch(encoder, decoder, device, dataloader, loss_fn, optimizer):
     
